72f944a134e550899317a9e8ae2c2fefe9e76756
foxy242 <114094487+foxy242@users.noreply.github.com>
2023-11-15 19:14:08 +0200

    1

--- nay.js
+++ nay.js
@@ -1,50 +1,51 @@
 // task 1
 
-// let nay = [1, 2, 3];
-// nay[1] = 10;
-// console.log(nay);
+let nay = [1, 2, 3];
+nay[1] = 10;
+console.log(nay);
 
 // Створити масив з трьох чисел. Змінити значення другого елемента масиву на 10.
 
 // task 2
 
-// let nay2 = ["Georgiy", "Ivan", "Olga"];
-// nay2[3] = "Tatiana";
-// console.log(nay);
+let nay2 = ["Georgiy", "Ivan", "Olga"];
+nay2[3] = "Tatiana";
+console.log(nay);
 
 // Створити масив із трьох рядків. Додати до масиву ще одну рядків.
 
-// task 3=====
+// task 3
 
-// const nay3 = [1, 2, 3];
-// const suma = 0
+const nay3 = [1, 2, 3];
+let suma = 0
 
-// for (const num of nay3) {
-//   console.log(num);
-// }
+for (let i = 0; i < nay3.length; i += 1) {
+suma += nay3[i]
+}
+console.log(suma)
 
 // Створити скрипт який поверне суму всіх чисел в масиві.
 
 // task 4
 
-// const nay6 = ["1", "2", "3"];
+const nay6 = ["1", "2", "3"];
 
-// for (let i = 0; i < nay6.length; i += 1) {
-//   console.log(nay6[i]);
-// }
+for (let i = 0; i < nay6.length; i += 1) {
+  console.log(nay6[i]);
+}
 
 // Створити масив з 5-ти чисел. Вивести на екран всі елементи масиву за допомогою циклу for.
 
 // task 5
 
 
-// const nay5 = ["Georgiy", "Ivanovich", "Tatiana"];
+const nay5 = ["Georgiy", "Ivanovich", "Tatiana"];
 
-// for (let i = 0; i < nay5.length; i += 1) {
-//   if (i % 5 === 0) {
-//   console.log(nay5[i]);
-//   }
-// }
+for (let i = 0; i < nay5.length; i += 1) {
+  if (i % 5 === 0) {
+  console.log(nay5[i]);
+  }
+}
 
 
 // Створити масив із 5-ти рядків. Вивести на екран кожен рядок з масиву, який містить більше 5-ти символів.
@@ -68,12 +69,12 @@ for (let i = 0; i < nay7.length; i += 1) {
 
 // task 7
 
-// const nay9 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const nay9 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// for (let i = 0; i <= 10; i += 1) {
-//   if (i %2 ===0) {
-//   console.log(i);
-//   }
-// }
+for (let i = 0; i <= nay9.length; i += 1) {
+  if (i %2 ===0) {
+  console.log(i);
+  }
+}
 
 // Створити масив з 10-ти чисел. Знайти всі парні числа в масиві та вивести їх на екран.
